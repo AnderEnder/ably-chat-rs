7562d371f46977f4f4c3aef1021a23a4c6c1c613
Andrii Radyk <ander.ender@gmail.com>
2026-07-23 18:14:55 +0200

    feat(messages): update (full-replace) message

--- crates/ably-chat-rs/src/lib.rs
+++ crates/ably-chat-rs/src/lib.rs
@@ -27,7 +27,7 @@ mod room;
 pub use room::Room;
 
 mod messages;
-pub use messages::{GetMessage, History, Messages, SendMessage, Versions};
+pub use messages::{GetMessage, History, Messages, SendMessage, UpdateMessage, Versions};
 
 mod reactions;
 pub use reactions::{ClientReactions, Reactions};
